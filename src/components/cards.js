c091b2d2ef8cc9b7ad6c4006d0d2499c906ab399
Sana Abid <82564648+Sana-21@users.noreply.github.com>
2023-04-15 12:09:02 +0500

    Added cards.js component

---
+++ src/components/cards.js
@@ -0,0 +1,17 @@
+import card from "../resources/cards.svg"
+function Cards(){
+    return(
+        <div className="Card-Info">
+            <h1>Cards</h1>
+            <div className="card-details">
+                <img src = {card} alt = "card"></img>
+            </div>
+            <div className="c-btns">
+                <button className = "btns c-det-btn">View Details</button>
+                <button className = "btns c-manage-btn">Manage Cards</button>
+            </div>
+        </div>
+    );
+}
+
+export default Cards;
