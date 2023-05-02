4d49f925d870bd86e88ca7882d6ca84b34f85443
Noureen Fatima <karissa.kaal@gmail.com>
2023-05-02 20:05:28 +0500

    Fixed Design of Sign Up page

--- src/components/LoginSignup.js
+++ src/components/LoginSignup.js
@@ -1,5 +1,4 @@
 import facerecg from "../resources/face-recg.svg"
-import "../login-style.css"
 import React from 'react';
 import { useState } from 'react';
 
@@ -15,44 +14,46 @@ function LoginSignup() {
     };
 
     return (
-    <div className="form-structor">
-        <div className={`signup ${!isLogin ? 'slide-up' : ''}`}>
-        <h2 className="form-title" id="signup" onClick={handleSignupClick}>
-            Sign up
-        </h2>
-        <form onSubmit={handleSubmit}>
-            <div className="form-holder">
-            <input type="text" className="input" placeholder="Name" />
-            <input type="cnic" className="input" placeholder="CNIC" />
-            <input type="card" className="input" placeholder="Debit/Credit Card" />
-            </div>
+        <div className="container-lg">
+            <div className="form-structor">
+                <div className={`signup ${!isLogin ? 'slide-up' : ''}`}>
+                    <h2 className="form-title" id="signup" onClick={handleSignupClick}>
+                        Sign up
+                    </h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-holder">
+                        <input type="text" className="input" placeholder="Name" />
+                        <input type="cnic" className="input" placeholder="CNIC" />
+                        <input type="card" className="input" placeholder="Debit/Credit Card" />
+                        </div>
 
-            <div className="face-recog-svg">
-            <img src={facerecg} alt="face recognition" />
-            </div>
-            <button type="submit" className="submit-btn">Sign up</button>
-        </form>
-        </div>
+                        <div className="face-recog-svg">
+                        <img src={facerecg} alt="face recognition" />
+                        </div>
+                        <button type="submit" className="submit-btn">Sign up</button>
+                    </form>
+                </div>
 
-        <div className={`login ${isLogin ? 'slide-up' : ''}`}>
-        <div className="center">
-            <h2 className="form-title" id="login" onClick={handleLoginClick}>
-            Log in
-            </h2>
-            <form onSubmit={handleSubmit}>
-            <div className="form-holder">
-                <input type="login-id" className="input" placeholder="Login ID" />
-                <input type="password" className="input" placeholder="Password" />
+                <div className={`login ${isLogin ? 'slide-up' : ''}`}>
+                    <div className="center">
+                        <h2 className="form-title" id="login" onClick={handleLoginClick}>
+                        Log in
+                        </h2>
+                        <form onSubmit={handleSubmit}>
+                        <div className="form-holder">
+                            <input type="login-id" className="input" placeholder="Login ID" />
+                            <input type="password" className="input" placeholder="Password" />
+                        </div>
+                        <button type="submit" className="submit-btn">Log in</button>
+                        <hr/>
+                        <div className="face-recog-svg">
+                            <img src={facerecg} alt="face recognition" />
+                        </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <button type="submit" className="submit-btn">Log in</button>
-            <hr/>
-            <div className="face-recog-svg">
-                <img src={facerecg} alt="face recognition" />
-            </div>
-            </form>
         </div>
-        </div>
-    </div>
     );
 }
 
