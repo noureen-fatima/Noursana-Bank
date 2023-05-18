37c27cc7073c42ff5abb6e8f3b6909cdbca18fb4
Noureen Fatima <karissa.kaal@gmail.com>
2023-05-18 10:58:23 +0500

    changes

--- src/components/Bene-manage.js
+++ src/components/Bene-manage.js
@@ -1,51 +1,112 @@
-import profileLabel from "../resources/profile-label.svg"
-import edit from "../resources/edit.svg"
-import del from "../resources/delete.svg"
-import React from 'react';
-function BeneManage()
-{
+import React, { useState, useEffect } from 'react';
+import profileLabel from "../resources/profile-label.svg";
+import { makeApiRequest } from '../api/api';
+
+function BeneManage() {
+  const [showAddBeneficiary, setShowAddBeneficiary] = useState(false);
+  const [accountNumber, setAccountNumber] = useState(0);
+  const [beneficiaryName, setBeneficiaryName] = useState('');
+  const [benies, setBenies] = useState([]);
+  const [acc, setAcc] = useState(0);
+
+  useEffect(() => {
+    const fetchAcc = async () => {
+        try {
+            const url = '/getAcc';
+            const method = 'GET';
+            const body = {};
+            const response = await makeApiRequest(url, method, body);
+            setAcc(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const fetchBene = async () => {
+    try {
+        const url = '/getBeneCurr';
+        const method = 'GET';
+        const body = {};
+        const response = await makeApiRequest(url, method, body);
+        setBenies(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+    };
+
+    fetchBene();
+    fetchAcc();
+
+    });
+  
+  const handleAddBeneficiary = () => {
+    setShowAddBeneficiary(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const postBene = async () => {
+        try {
+            const url = '/addBeneficiary';
+            const method = 'POST';
+            const body = {"senderAccountNumber":acc, "beneficiaryName":beneficiaryName, "beneficiaryAccountNumber":accountNumber};
+            console.log(acc, beneficiaryName, accountNumber);
+            const response = await makeApiRequest(url, method, body);
+            setShowAddBeneficiary(false);
+            setAccountNumber('');
+            setBeneficiaryName('');
+            alert("Beneficiary Added");
+            return response;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    postBene();
+}
     return(
-        <div className="BeneManage">
-             <h2 className="b-heading b-m-heading">Manage Beneficiary List</h2>
-             <div className="b-l-btns b-m-btns"><button className="btns b-l-sort">Sort</button> <button className="btns b-l-add">Add</button></div>
-             <div className="b-m-list">
-                    <ul className="b-ulist">
-                        <li className="b-list-item bm-li"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                        <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div><div><img className="edit-svg" src={edit}/>
-                        <img className="del-svg" src={del}/></div>
-                        </li>
-                        <li className="b-list-item bm-li"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                        <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div><div><img className="edit-svg" src={edit}/>
-                        <img className="del-svg" src={del}/></div>
-                        </li>
-                        <li className="b-list-item bm-li"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                        <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div><div><img className="edit-svg" src={edit}/>
-                        <img className="del-svg" src={del}/></div>
-                        </li>
-                        <li className="b-list-item bm-li"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                        <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div><div><img className="edit-svg" src={edit}/>
-                        <img className="del-svg" src={del}/></div>
-                        </li>
-                        <li className="b-list-item bm-li"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                        <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div><div><img className="edit-svg" src={edit}/>
-                        <img className="del-svg" src={del}/></div>
-                        </li>
-                        <li className="b-list-item bm-li"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                        <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div><div><img className="edit-svg" src={edit}/>
-                        <img className="del-svg" src={del}/></div>
-                        </li>
-                        <li className="b-list-item bm-li"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                        <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div><div><img className="edit-svg" src={edit}/>
-                        <img className="del-svg" src={del}/></div>
-                        </li>
-                        <li className="b-list-item bm-li"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                        <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div><div><img className="edit-svg" src={edit}/>
-                        <img className="del-svg" src={del}/></div>
-                        </li>
-                        
-                    </ul>
-                 </div>
+        <>
+        <div className="b-l-super">
+        <h2 className="b-heading">Managing Beneficiary List</h2>
+        <div className="b-l-btns">
+          {!showAddBeneficiary && (
+            <button className="btns b-l-add" onClick={handleAddBeneficiary}>
+              Add
+            </button>
+          )}
         </div>
+
+        {showAddBeneficiary && (
+          <form className="add-beneficiary-form" onSubmit={handleSubmit}>
+            <div className="add-beneficiary-inputs">
+              <input
+                type="number"
+                placeholder="Account Number"
+                className="amt-send-input"
+                value={accountNumber}
+                onChange={(e) => setAccountNumber(e.target.valueAsNumber)}
+              />
+              <input
+                type="text"
+                className="amt-send-input"
+                placeholder="Name"
+                value={beneficiaryName}
+                onChange={(e) => setBeneficiaryName(e.target.value)}
+              />
+            </div>
+            <button className="btns b-l-add" type="submit">Submit</button>
+          </form>
+        )}
+
+                <ul className="b-ulist">
+                    {benies.map((b) => (
+                        <li className="b-list-item"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">{b.name}</div>
+                        <div className="B-list-acc">{b.accountNumber}</div>
+                        </li>
+                    ))}
+                </ul>
+           </div>
+           </>
     );
 
 }
--- src/components/SendMoney.js
+++ src/components/SendMoney.js
@@ -1,65 +1,191 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import profileLabel from "../resources/profile-label.svg";
+import { makeApiRequest } from '../api/api';
 
-function SendMoney(){
+function SendMoney() {
+  const [showAddBeneficiary, setShowAddBeneficiary] = useState(false);
+  const [accountNumber, setAccountNumber] = useState(0);
+  const [beneficiaryName, setBeneficiaryName] = useState('');
+  const [benies, setBenies] = useState([]);
+  const [acc, setAcc] = useState(0);
+  const [amountToSend, setAmountToSend] = useState('');
 
-    return(
-        <>
-            <div className="SendMoney">
-                <h2>Send Money</h2>
-                <div className="from-acc">
-                    <label for = "input-from-acc">From Account</label>
-                    <input className = "input-from-acc"type = "text"></input>
-                </div>
+  useEffect(() => {
+    const fetchAcc = async () => {
+      try {
+        const url = '/getAcc';
+        const method = 'GET';
+        const body = {};
+        const response = await makeApiRequest(url, method, body);
+        setAcc(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-                <div className="to-acc">
-                    <label for = "input-to-acc">To Account</label>
-                    <input className="input-to-acc" type = "text"></input>
-                </div>
-            
-                <div className="amt-to-send">
-                    <label for = "amt-send-input">Amount</label>
-                    <input className="amt-send-input" type = "text"></input>
-                </div>
+    const fetchBene = async () => {
+      try {
+        const url = '/getBeneCurr';
+        const method = 'GET';
+        const body = {};
+        const response = await makeApiRequest(url, method, body);
+        setBenies(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-                <br></br>
-                <button className="btns send-btn">Send</button>
+    fetchBene();
+    fetchAcc();
+  }, []);
+
+  const handleAddBeneficiary = () => {
+    setShowAddBeneficiary(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const postBene = async () => {
+      try {
+        const url = '/addBeneficiary';
+        const method = 'POST';
+        const body = {
+          senderAccountNumber: acc,
+          beneficiaryName: beneficiaryName,
+          beneficiaryAccountNumber: accountNumber,
+        };
+        const response = await makeApiRequest(url, method, body);
+        setShowAddBeneficiary(false);
+        setAccountNumber('');
+        setBeneficiaryName('');
+        alert('Beneficiary Added');
+        return response;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    postBene();
+  };
+
+  const handleBeneficiaryClick = (accountNumber) => {
+    setAccountNumber(accountNumber);
+  };
+
+  const transaction = async () => {
+    try {
+      const url = '/transaction';
+      const method = 'POST';
+      const body = {
+        senderAccNo: acc,
+        recipientAccNo: accountNumber,
+        amount: amountToSend,
+      };
+      const response = await makeApiRequest(url, method, body);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="SendMoney">
+        <h2>Send Money</h2>
+        <div className="from-acc">
+          <label htmlFor="input-from-acc">From Account</label>
+          <input
+            className="input-from-acc"
+            type="text"
+            id="input-from-acc"
+            placeholder={acc}
+            value={acc}
+          ></input>
+        </div>
+
+        <div className="to-acc">
+          <label htmlFor="input-to-acc">To Account</label>
+          <input
+            className="input-to-acc"
+            type="text"
+            id="input-to-acc"
+            value={accountNumber}
+            onChange={(e) => setAccountNumber(e.target.value)}
+          ></input>
+        </div>
+
+        <div className="amt-to-send">
+          <label htmlFor="amt-send-input">Amount</label>
+          <input
+            className="amt          -send-input"
+            type="text"
+            id="amt-send-input"
+            value={amountToSend}
+            onChange={(e) => setAmountToSend(e.target.value)}
+          ></input>
+        </div>
+
+        <br />
+        <button className="btns send-btn" onClick={transaction}>
+          Send
+        </button>
+      </div>
+
+      <div className="b-l-super">
+        <h2 className="b-heading">Beneficiary List</h2>
+        <div className="b-l-btns">
+          {!showAddBeneficiary && (
+            <button className="btns b-l-add" onClick={handleAddBeneficiary}>
+              Add
+            </button>
+          )}
+        </div>
+        {showAddBeneficiary && (
+          <form className="add-beneficiary-form" onSubmit={handleSubmit}>
+            <div className="add-beneficiary-inputs">
+              <input
+                type="number"
+                placeholder="Account Number"
+                className="amt-send-input"
+                value={accountNumber}
+                onChange={(e) => setAccountNumber(e.target.valueAsNumber)}
+              />
+              <input
+                type="text"
+                className="amt-send-input"
+                placeholder="Name"
+                value={beneficiaryName}
+                onChange={(e) => setBeneficiaryName(e.target.value)}
+              />
             </div>
-            <div className="b-l-super">
-                <h2 className="b-heading">Beneficiary List</h2>
-                <div className="b-l-btns"><button className="btns b-l-add">Add</button></div>
-                <div className="B-list">
-                    <ul className="b-ulist">
-                    <li className="b-list-item"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                    <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div>
-                    </li>
-                    <li className="b-list-item"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                    <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div>
-                    </li>
-                    <li className="b-list-item"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                    <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div>
-                    </li>
-                    <li className="b-list-item"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                    <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div>
-                    </li>
-                    <li className="b-list-item"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                    <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div>
-                    </li>
-                    <li className="b-list-item"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                    <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div>
-                    </li>
-                    <li className="b-list-item"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                    <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div>
-                    </li>
-                    <li className="b-list-item"><img className="profile-svg" src={profileLabel} alt="Accounts" /> <div className="B-list-item">User 1</div>
-                    <div className="B-list-acc">003944094094930</div><div className="currency">PKR</div>
-                    </li>
-                    </ul>
-                </div>
-           </div>
-           </>
+            <button className="btns b-l-add" type="submit">
+              Submit
+            </button>
+          </form>
+        )}
 
-    );
+        <ul className="b-ulist">
+          {benies.map((b) => (
+            <li
+              key={b.accountNumber}
+              className="b-list-item"
+              onClick={() => handleBeneficiaryClick(b.accountNumber)}
+            >
+              <img
+                className="profile-svg"
+                src={profileLabel}
+                alt="Accounts"
+              />
+              <div className="B-list-item">{b.name}</div>
+              <div className="B-list-acc">{b.accountNumber}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default SendMoney;
+
